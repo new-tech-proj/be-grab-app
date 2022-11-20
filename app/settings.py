00f50ab91395c2b640f737e17806e5a5088819fe
Phạm Thành Trung <thanhtrungit2001@gmail.com>
2022-11-20 23:49:49 +0700

    add route get posts

--- app/settings.py
+++ app/settings.py
@@ -5,4 +5,5 @@ PGPASSWORD = os.getenv("PGPASSWORD", default="Trung2001")
 PGHOST = os.getenv("PGHOST", default="localhost")
 PGPORT = os.getenv("PGPORT", default="5432")
 PGDATABASE = os.getenv("PGDATABASE", default="grab")
-DATABASE_URL = f"postgresql://{PGUSER}:{PGPASSWORD}@{PGHOST}:{PGPORT}/{PGDATABASE}"
+# DATABASE_URL = f"postgresql://{PGUSER}:{PGPASSWORD}@{PGHOST}:{PGPORT}/{PGDATABASE}"
+DATABASE_URL = "postgresql://postgres:L6SgAq2wu1jkYr5SqjIl@containers-us-west-106.railway.app:6622/railway"
