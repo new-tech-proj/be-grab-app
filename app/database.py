6705d7aabb3e87da43da54f38b8f9fd021b6cf0d
Phạm Thành Trung <thanhtrungit2001@gmail.com>
2022-11-20 15:28:28 +0700

    add route login

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 from app.settings import DATABASE_URL
 
@@ -8,5 +8,5 @@ from app.settings import DATABASE_URL
 engine = create_engine(DATABASE_URL, echo=False)
 metadata = MetaData(bind=engine)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session = Session(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- app/views.py
+++ app/views.py
@@ -1,6 +1,39 @@
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
 from app import app
+from app.models import User, Post
+from app.database import session
+from app.utils import verify_password, make_respones
 
 
 @app.get("/")
 def index():
     return "Hello, world!"
+
+
+@app.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    username = form_data.username
+    password = form_data.password
+
+    user = session.query(User).filter(User.username == username).first()
+    
+    if user is not None:
+        if verify_password(password, user.hashed_password):
+            print(user.id)
+            return make_respones(
+                status_code=0,
+                message="Login successful",
+                data={
+                    "user_id": user.id
+                }
+            )
+        else:
+            return make_respones(
+                message="Wrong username or password"
+            )
+    else:
+        return make_respones(
+            message="Wrong username or password"
+        )
