5452976a2bfed2797b15c559355ac41df0e48c8f
Phạm Thành Trung <thanhtrungit2001@gmail.com>
2022-11-21 04:00:49 +0700

    fix error port type

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     config = uvicorn.Config(
         app=app,
         host="0.0.0.0",
-        port=os.getenv("PORT", default=5000),
+        port=int(os.getenv("PORT", default=5000)),
         log_level="info"
     )
     server = uvicorn.Server(config)
